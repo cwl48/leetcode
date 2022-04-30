5e10da53630bef0bb0b7361d9273fd1f8e6eebab
zhilai <shenxy@zdtz168.com>
2022-04-30 21:47:19 +0800

    1.two sum

--- src/main/java/com/chaoyer/leetcode/editor/cn/TwoSum.java
+++ src/main/java/com/chaoyer/leetcode/editor/cn/TwoSum.java
@@ -62,7 +62,14 @@ public class TwoSum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] twoSum(int[] nums, int target) {
-
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i+1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i,j};
+                    }
+                }
+            }
+            return new int[]{};
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
